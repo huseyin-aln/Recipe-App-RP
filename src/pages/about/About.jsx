75f0e7013ff02d8eedcd7c8e49f0fe49226c6415
huseyin <huseyinarslan06@gmail.com>
2022-07-15 12:03:23 +0300

    aboutstyle

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,19 +8,18 @@ const About = () => {
       <StyledImage src={codingSvg} />
       <div>
         <h1>
-          About Software Developer <span>Josh Smith</span>
+          About Software Developer <span>ME</span>
         </h1>
       </div>
       <InfoContainer>
-        <h2>Hi, I'am Josh</h2>
+        <h2>Hi</h2>
         <h3>I'm currently learning Full-Stack Development Languages.</h3>
         <h4>
           I've already known JS, ReactJS, ReactNative, NodeJS, MongoDB,SQL,
           Python, AWS Services.
         </h4>
         <h2>
-          <a href="mailto:josh.smith@clarusway.com">Send email</a> :
-          josh.smith@clarusway.com
+          <a href="mailto:js@cw.com">Send email</a> : js@cw.com
         </h2>
       </InfoContainer>
     </AboutContainer>
